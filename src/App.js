500a114a031da39d61dda39cc274d60ea8aebfca
adobias7811 <adobias11@gmail.com>
2021-08-13 13:23:24 -0400

    Added search engine + footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      React Dictionary App
+      <div className="container">
+        <header className="App-header">React Dictionary App</header>
+        <main>
+          <Dictionary />
+        </main>
+      </div>
+      <footer className="App-footer">
+        Coded by{" "}
+        <a
+          href="https://silly-kepler-6e4998.netlify.app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Amanda Dobias
+        </a>{" "}
+        and open sourced on{" "}
+        <a
+          href="https://github.com/adobias7811"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </footer>
     </div>
   );
 }
